5b1efc2f40439824acb53128134785693303af90
RusithTharindu <rusiththarindu2@gmail.com>
2026-05-01 18:59:24 +0530

    fix: correct HTML entity usage in AboutCard component

--- src/components/molecules/AboutCard.tsx
+++ src/components/molecules/AboutCard.tsx
@@ -12,12 +12,13 @@ export function AboutCard() {
             where the API meets the UI, where trust assumptions break, where
             abstractions leak
           </strong>
-          . Currently pursuing a Master's in Cyber Security, which mostly means
-          I've started reading every system like something will go wrong.
+          . Currently pursuing a Master&apos;s in Cyber Security, which mostly
+          means I&apos;ve started reading every system like something will go
+          wrong.
         </p>
         <p>
-          I'm at my best collaborating on hard problems with people who have
-          opinions. Good software is a team argument that ends well.
+          I&apos;m at my best collaborating on hard problems with people who
+          have opinions. Good software is a team argument that ends well.
         </p>
       </div>
     </MotionCard>
